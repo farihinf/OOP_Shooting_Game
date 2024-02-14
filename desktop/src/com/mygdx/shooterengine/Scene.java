4076646691fdc1d2556adb0760176d0fdf68dea9
MiloyBlitz <77763258+MiloyBlitz@users.noreply.github.com>
2024-02-14 16:02:13 +0800

    Add files via upload

---
+++ desktop/src/com/mygdx/shooterengine/Scene.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Screen;
+
+public abstract class Scene implements Screen
+{
+    public void resize(int width, int height) 
+    {
+    	
+	}
+    
+    public void show() 
+    {
+    	
+	}
+    
+    public void hide() 
+    {
+    	
+	}
+    
+    public void pause() 
+    {
+    	
+	}
+    
+    public void resume() 
+    {
+    	
+	}
+    
+    public void dispose() 
+    {
+    	
+	}
+}
