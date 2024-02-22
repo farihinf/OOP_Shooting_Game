a3aa19ec48effdd2dfc4a62058a63d544cd83834
smolfroggo <145501461+smolfroggo@users.noreply.github.com>
2024-02-22 15:09:51 +0800

    Update AudioAsset.java

--- desktop/src/com/mygdx/shooterengine/AudioAsset.java
+++ desktop/src/com/mygdx/shooterengine/AudioAsset.java
@@ -1,10 +1,10 @@
 package com.mygdx.shooterengine;
 
+//Interface for Audio
 public interface AudioAsset {
 
     public void PlayMusic(String filePath);
     public void StopMusic();
     public void PlaySound(String filePath);
-    //public void 
     
 }
