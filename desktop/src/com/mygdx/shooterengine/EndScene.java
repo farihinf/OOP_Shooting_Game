75a48fa766d5b3faebec8595a0c2f08276bb1c84
farihinf <farihinfatten04@gmail.com>
2024-03-14 22:03:58 +0800

    temp fix for main menu button after end scene

--- desktop/src/com/mygdx/shooterengine/EndScene.java
+++ desktop/src/com/mygdx/shooterengine/EndScene.java
@@ -46,7 +46,9 @@ public class EndScene extends Scene
         returntoMainMenuButton = new Texture(Gdx.files.internal("ScreenImages\\ReturnToMainMenu.png"));
 
         returntoMainMenuButtonPosX = (SCENE_WIDTH - BUTTON_WIDTH) / 2;
-        returntoMainMenuButtonPosY = (SCENE_HEIGHT / 2) - 10 - BUTTON_HEIGHT;
+        // returntoMainMenuButtonPosY = (SCENE_HEIGHT / 2) - 10 - BUTTON_HEIGHT;
+        returntoMainMenuButtonPosY = (SCENE_HEIGHT / 2) + 20 - BUTTON_HEIGHT;
+
     }
 
     public void draw() 
